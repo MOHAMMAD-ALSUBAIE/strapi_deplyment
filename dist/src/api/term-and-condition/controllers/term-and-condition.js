d68ed20f202e95d86ae4f99c77afca56af60d37d
Mohammed Alsubaie <alsubaiemoh672@gmail.com>
2026-09-04 01:25:26 +0300

    .

--- dist/src/api/term-and-condition/controllers/term-and-condition.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * term-and-condition controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::term-and-condition.term-and-condition');
--- dist/src/api/term-and-condition/services/term-and-condition.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * term-and-condition service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::term-and-condition.term-and-condition');
