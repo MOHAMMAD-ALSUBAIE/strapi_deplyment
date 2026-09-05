bb7f301c7ac4dfb0950911d7393276cf151b6f7e
Mohammed Alsubaie <alsubaiemoh672@gmail.com>
2026-09-05 17:01:24 +0300

    .

--- .github/workflows/deply.sh
+++
@@ -1,9 +0,0 @@
-            docker pull "$DOCKER_USERNAME/strapi-deployment:latest"
-            docker stop projects-strapi-1 || true
-            docker rm projects-strapi-1 || true
-            docker run -d --name projects-strapi-1 \
-              --network projects_default \
-              --env-file /srv/strapi/.env \
-              -p 1337:1337 \
-              "$DOCKER_USERNAME/strapi-deployment:latest"
-            docker image prune -f
